a335bcbadd920369ac5062e5761e6af9478c4a75
Aqshol <aafid0127@pnm.co.id>
2024-08-10 19:23:16 +0700

    revised Layout

--- remote/src/components/base/Layout.tsx
+++ remote/src/components/base/Layout.tsx
@@ -6,7 +6,8 @@ import {
   UserOutlined,
   VideoCameraOutlined,
 } from "@ant-design/icons";
-import { Button, Layout as AntdLayout, Menu, theme } from "antd";
+import { Button, Layout as AntdLayout, Menu, theme, Typography } from "antd";
+import { ElementObjectCssStyle } from "../../types/general";
 
 type Props = {
   children: React.ReactNode;
@@ -14,66 +15,82 @@ type Props = {
 
 export default function Layout(props: Props) {
   const { Header, Sider, Content } = AntdLayout;
-  const [collapsed, setCollapsed] = useState(false);
   const {
-    token: { colorBgContainer, borderRadiusLG },
+    token: { colorBgContainer, borderRadiusLG, ...antdToken },
   } = theme.useToken();
 
+  const STYLE = {
+    AntdLayout: {
+      minHeight: "100vh",
+    },
+    TitleLayout: {
+      color: antdToken.colorWhite,
+      textAlign: "center",
+      marginTop: "16px",
+    },
+    HeaderLayout: {
+      padding: 0,
+      background: colorBgContainer,
+    },
+
+    ButtonCollapseLayout: {
+      fontSize: "16px",
+      width: 64,
+      height: 64,
+    },
+
+    ContentLayout: {
+      margin: "24px 16px",
+      padding: 24,
+      minHeight: 280,
+      background: colorBgContainer,
+      borderRadius: borderRadiusLG,
+    },
+  } satisfies ElementObjectCssStyle;
+
+  const MENU_LIST = [
+    {
+      key: "1",
+      icon: <UserOutlined />,
+      label: "nav 1",
+    },
+    {
+      key: "2",
+      icon: <VideoCameraOutlined />,
+      label: "nav 2",
+    },
+    {
+      key: "3",
+      icon: <UploadOutlined />,
+      label: "nav 3",
+    },
+  ];
+
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
-    <AntdLayout
-      style={{
-        minHeight: "100vh",
-      }}
-    >
+    <AntdLayout style={STYLE.AntdLayout}>
       <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="demo-logo-vertical" />
+        <Typography.Title style={STYLE.TitleLayout} level={3}>
+          Dash
+        </Typography.Title>
         <Menu
           theme="dark"
           mode="inline"
           defaultSelectedKeys={["1"]}
-          items={[
-            {
-              key: "1",
-              icon: <UserOutlined />,
-              label: "nav 1",
-            },
-            {
-              key: "2",
-              icon: <VideoCameraOutlined />,
-              label: "nav 2",
-            },
-            {
-              key: "3",
-              icon: <UploadOutlined />,
-              label: "nav 3",
-            },
-          ]}
+          items={MENU_LIST}
         />
       </Sider>
       <AntdLayout>
-        <Header style={{ padding: 0, background: colorBgContainer }}>
+        <Header style={STYLE.HeaderLayout}>
           <Button
             type="text"
             icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
             onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: "16px",
-              width: 64,
-              height: 64,
-            }}
+            style={STYLE.ButtonCollapseLayout}
           />
         </Header>
-        <Content
-          style={{
-            margin: "24px 16px",
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
-          }}
-        >
-          {props.children}
-        </Content>
+        <Content style={STYLE.ContentLayout}>{props.children}</Content>
       </AntdLayout>
     </AntdLayout>
   );
